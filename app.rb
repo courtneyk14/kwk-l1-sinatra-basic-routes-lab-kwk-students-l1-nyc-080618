09af01d8468bb8922dad50119c41592c2bc4866c
courtneyk14 <cbrookelley14@gmail.com>
2018-08-14 17:17:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "Hello my name is Courtney!"
+end
+
+
+get "/hometown" do
+  "My hometown is Monroe."
+end
+get "/favorite-song" do
+  "My favorite song is young and beautiful"
+end
 end
